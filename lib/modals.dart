9581e50353b0709bac14bab2d069f0e58f2516b4
MuhammadEssam96 <muhammad.essam96@gmail.com>
2021-06-24 16:48:38 +0200

    Migrate to null-safety

--- lib/modals.dart
+++ lib/modals.dart
@@ -2,20 +2,31 @@ import 'package:flutter/material.dart';
 
 
 class SCBottomBarDetails {
-  Color backgroundColor;
-  double elevation;
-  List<Color> circleColors;
-  IconThemeData activeIconTheme;
-  IconThemeData iconTheme;
-  TextStyle activeTitleStyle;
-  TextStyle titleStyle;
-  List<SCBottomBarItem> items;
+  Color? backgroundColor;
+  double? elevation;
+  List<Color>? circleColors;
+  IconThemeData? activeIconTheme;
+  IconThemeData? iconTheme;
+  TextStyle? activeTitleStyle;
+  TextStyle? titleStyle;
+  List<SCBottomBarItem?> items;
   List<SCItem> circleItems;
-  SCActionButtonDetails actionButtonDetails;
-  double bnbHeight;
+  SCActionButtonDetails? actionButtonDetails;
+  double? bnbHeight;
 
-  SCBottomBarDetails(
-      {@required this.items, @required this.circleItems,this.bnbHeight,this.actionButtonDetails, this.activeIconTheme, this.iconTheme, this.activeTitleStyle, this.titleStyle, this.circleColors,this.backgroundColor,this.elevation});
+  SCBottomBarDetails({
+    required this.items,
+    required this.circleItems,
+    this.bnbHeight,
+    this.actionButtonDetails,
+    this.activeIconTheme,
+    this.iconTheme,
+    this.activeTitleStyle,
+    this.titleStyle,
+    this.circleColors,
+    this.backgroundColor,
+    this.elevation
+  });
 }
 
 class SCActionButtonDetails{
@@ -23,20 +34,33 @@ class SCActionButtonDetails{
   Icon icon;
   double elevation;
 
-  SCActionButtonDetails({@required this.color,@required this.icon,@required this.elevation});
+  SCActionButtonDetails({
+    required this.color,
+    required this.icon,
+    required this.elevation
+  });
 }
 
 class SCItem {
   Icon icon;
-  Function onPressed;
-  SCItem({@required this.icon, @required this.onPressed});
+  void Function() onPressed;
+
+  SCItem({
+    required this.icon,
+    required this.onPressed
+  });
 }
 
 class SCBottomBarItem {
-  IconData activeIcon;
+  IconData? activeIcon;
   IconData icon;
-  String title;
+  String? title;
   Function onPressed;
 
-  SCBottomBarItem({this.activeIcon, @required this.icon, this.title, @required this.onPressed});
+  SCBottomBarItem({
+    this.activeIcon,
+    required this.icon,
+    this.title,
+    required this.onPressed
+  });
 }
